11aebec81fc0646fc7b4cc50a640f826a9b6116d
Angela Torrealba <angvts12@gmail.com>
2025-07-21 23:48:00 -0400

    [Practice_01] Create a simple player with movement

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,31 +1,38 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using Animals;
 
 public class GameController : MonoBehaviour
 {
+    private Animal myDog;
+    private Animal myCat;
+    private void Awake()
+    {
+        Debug.Log("PRIMERO SE EJECUTA AWAKE");
+    }
+    
+
     // Start is called before the first frame update
     private void Start()
     {
         // Crear instancias de Dog y Cat
-        Animal myDog = new Dog();
+        myDog = new Dog();
         myDog.Name = "Rex";
         myDog.ServeFood();  // Llama al método de la clase Dog
         myDog.ShowFood();   // Muestra la comida
 
-        Animal myCat = new Cat();
+        myCat = new Cat();
         myCat.Name = "Miau";
         myCat.ServeFood();  // Llama al método de la clase Cat
         myCat.ShowFood();   // Muestra la comida
 
-        // Hacer que los animales hablen
-        myDog.Talk();
-        myCat.Talk();
+
     }
 
     // Update is called once per frame
     private void Update()
     {
-        
+        // Hacer que los animales hablen
+        myDog.Talk();
+        myCat.Talk();
     }
 }
--- Assets/Scripts/Sample.cs
+++ Assets/Scripts/Sample.cs
@@ -1,77 +1,81 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-// Clase base Animal
-public class Animal
+namespace Animals
 {
-    public string Name;                // Propiedad pública para almacenar el nombre del animal
-    private int _age;                   // Campo privado para almacenar la edad del animal
-    protected List<string> _food;      // Lista protegida que almacena la comida que puede consumir el animal
-
-    // Constructor de la clase Animal, inicializa la lista de comida
-    public Animal()
+    // Clase base Animal
+    public class Animal
     {
-        _food = new List<string>();
-    }
+        public string Name;                // Propiedad pública para almacenar el nombre del animal
+        private int _age;                   // Campo privado para almacenar la edad del animal
+        protected List<string> _food;      // Lista protegida que almacena la comida que puede consumir el animal
 
-    // Método virtual Talk, que puede ser sobrescrito por clases derivadas
-    public virtual void Talk()
-    {
-        Debug.Log("El animal hace un sonido");
-    }
-
-    // Método virtual ServeFood, que agrega comida a la lista
-    public virtual void ServeFood()
-    {
-        _food.Add("Carne");
-        _food.Add("Pollo");
-    }
-
-    // Método para mostrar la comida disponible en la lista
-    public void ShowFood()
-    {
-        string foodList = "Comida disponible: ";
-        foreach (var food in _food)
+        // Constructor de la clase Animal, inicializa la lista de comida
+        public Animal()
         {
-            foodList += food + ", ";
+            _food = new List<string>();
         }
-        Debug.Log(foodList);
+
+        // Método virtual Talk, que puede ser sobrescrito por clases derivadas
+        public virtual void Talk()
+        {
+            Debug.Log("El animal hace un sonido");
+        }
+
+        // Método virtual ServeFood, que agrega comida a la lista
+        public virtual void ServeFood()
+        {
+            _food.Add("Carne");
+            _food.Add("Pollo");
+        }
+
+        // Método para mostrar la comida disponible en la lista
+        public void ShowFood()
+        {
+            string foodList = "Comida disponible: ";
+            foreach (var food in _food)
+            {
+                foodList += food + ", ";
+            }
+            Debug.Log(foodList);
+        }
+    }
+
+    // Clase derivada Dog, hereda de Animal
+    public class Dog : Animal
+    {
+        // Sobrescribe el método ServeFood para agregar comida adicional para perros
+        public override void ServeFood()
+        {
+            base.ServeFood();  // Llama al método de la clase base (Animal)
+            _food.Add("Perrarina");  // Agrega un tipo de comida exclusivo para perros
+        }
+
+        // Sobrescribe el método Talk para que el perro haga su sonido característico
+        public override void Talk()
+        {
+            Debug.Log("Wof wof");
+        }
+    }
+
+    // Clase derivada Cat, hereda de Animal
+    public class Cat : Animal
+    {
+        // Sobrescribe el método ServeFood para agregar comida adicional para gatos
+        public override void ServeFood()
+        {
+            base.ServeFood();  // Llama al método de la clase base (Animal)
+            _food.Add("Gatarina");  // Agrega un tipo de comida exclusivo para gatos
+        }
+
+        // Sobrescribe el método Talk para que el gato haga su sonido característico
+        public override void Talk()
+        {
+            Debug.Log("Miau");
+        }
     }
 }
 
-// Clase derivada Dog, hereda de Animal
-public class Dog : Animal
-{
-    // Sobrescribe el método ServeFood para agregar comida adicional para perros
-    public override void ServeFood()
-    {
-        base.ServeFood();  // Llama al método de la clase base (Animal)
-        _food.Add("Perrarina");  // Agrega un tipo de comida exclusivo para perros
-    }
-
-    // Sobrescribe el método Talk para que el perro haga su sonido característico
-    public override void Talk()
-    {
-        Debug.Log("Wof wof");
-    }
-}
-
-// Clase derivada Cat, hereda de Animal
-public class Cat : Animal
-{
-    // Sobrescribe el método ServeFood para agregar comida adicional para gatos
-    public override void ServeFood()
-    {
-        base.ServeFood();  // Llama al método de la clase base (Animal)
-        _food.Add("Gatarina");  // Agrega un tipo de comida exclusivo para gatos
-    }
-
-    // Sobrescribe el método Talk para que el gato haga su sonido característico
-    public override void Talk()
-    {
-        Debug.Log("Miau");
-    }
-}
 
 
 
